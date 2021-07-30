ada9780443e19d264ab11e428e5992e7dc0d0d73
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2021-07-30 12:27:28 +0100

    Scaffolding
    Set up scaffolding for CRUD functionality on the frontend - views and controllers setup to match model on the backend.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,6 @@ namespace AddressBook.Data
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
